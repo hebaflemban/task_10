d8a4ee058ec102488834ee190a77632c6005802b
Heba <36724067+heba@users.noreply.github.com>
2020-09-07 10:40:23 +0300

    detaled items

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
@@ -9,7 +9,7 @@ class Restaurant(models.Model):
     logo = models.ImageField(upload_to='restaurant_logos', null=True, blank=True)
 
     # Adding a relationship field
-    owner = models.ForeignKey(User, on_delete = models.CASCADE, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete = models.SET_DEFAULT, default=1 )
 
     def __str__(self):
     	return self.name
@@ -17,7 +17,7 @@ class Restaurant(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=400)
+    description = models.TextField()
     price = models.FloatField()
     restaurant = models.ForeignKey(Restaurant, on_delete = models.SET_NULL, blank=True, null=True)
 
